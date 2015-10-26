6df629abf31db7e34c11a715c6e1894ad386146c
TheBlackBloodyUnicorn <TheBlackBloodyUnicorn@hotmail.com>
2015-10-26 13:52:45 +0000

    hello Sector

---
+++ helloSector.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<htlm>
+    <head>
+    </head>
+
+    <body>
+        <p>Where do you want to say hello ?</p>
+        <p><a href="http://http://blackunicorn.azurewebsites.net/helloPrinter.php?query=Mars">Mars</a></p>
+        <p><a href="http://http://blackunicorn.azurewebsites.net/helloPrinter.php?query=Earth">Earth</a></p>
+        <p><a href="http://http://blackunicorn.azurewebsites.net/helloPrinter.php?query=Uranus">Uranus</a></p>
+    <?php
+
+
+
+    ?>
+
+    </body>
+
+</htlm>
