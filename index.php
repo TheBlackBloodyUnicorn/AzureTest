971b4d19f43d11e01a2604b54cc0a8d8b57d9f6e
TheBlackBloodyUnicorn <TheBlackBloodyUnicorn@hotmail.com>
2015-10-26 15:23:22 +0000

    couocu

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<htlm>
+    <head>
+    </head>
+
+    <body>
+
+    <p><a href="http://blackunicorn.azurewebsites.net/helloSector.php?query=before2010">•Movies created before 2010</a></p>
+    <p><a href="http://blackunicorn.azurewebsites.net/helloSector.php?query=marvelstudio">•Movies created by Marvel Studios</a></p>
+    <p><a href="http://blackunicorn.azurewebsites.net/helloSector.php?query=after2010">•Movies created after 2010</a></p>
+    <p><a href="http://blackunicorn.azurewebsites.net/helloSector.php?query=oscar">•Movies that mention an Oscar in the comments</a></p>
+
+        <?php
+
+
+
+        ?>
+
+    </body>
+
+</htlm>
