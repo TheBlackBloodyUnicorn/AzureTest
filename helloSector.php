ab8434a3a500e48014d5c883b83e2cc698f902d0
TheBlackBloodyUnicorn <TheBlackBloodyUnicorn@hotmail.com>
2015-10-26 13:56:45 +0000

    correction

--- helloSector.php
+++ helloSector.php
@@ -5,9 +5,9 @@
 
     <body>
         <p>Where do you want to say hello ?</p>
-        <p><a href="http://http://blackunicorn.azurewebsites.net/helloPrinter.php?query=Mars">Mars</a></p>
-        <p><a href="http://http://blackunicorn.azurewebsites.net/helloPrinter.php?query=Earth">Earth</a></p>
-        <p><a href="http://http://blackunicorn.azurewebsites.net/helloPrinter.php?query=Uranus">Uranus</a></p>
+        <p><a href="http://blackunicorn.azurewebsites.net/helloPrinter.php?query=Mars">Mars</a></p>
+        <p><a href="http://blackunicorn.azurewebsites.net/helloPrinter.php?query=Earth">Earth</a></p>
+        <p><a href="http://blackunicorn.azurewebsites.net/helloPrinter.php?query=Uranus">Uranus</a></p>
     <?php
 
 
