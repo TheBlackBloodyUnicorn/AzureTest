fecd902d8c89fdea4944d3eeba9b418189096104
TheBlackBloodyUnicorn <TheBlackBloodyUnicorn@hotmail.com>
2015-10-26 15:39:43 +0000

    couocu

--- helloSector.php
+++ helloSector.php
@@ -3,21 +3,6 @@
     <head>
     </head>
 
-    <body>
-        <p>Where do you want to say hello ?</p>
-        <p><a href="http://blackunicorn.azurewebsites.net/helloPrinter.php?query=Mars">Mars</a></p>
-        <p><a href="http://blackunicorn.azurewebsites.net/helloPrinter.php?query=Earth">Earth</a></p>
-        <p><a href="http://blackunicorn.azurewebsites.net/helloPrinter.php?query=Uranus">Uranus</a></p>
-
-        <form action="helloPrinter.php" method="post">
-            <label>Forename</label><input type="text" name="forename">
-            <label>Surname</label><input type="text" name="surname">
-            <input type="radio" name="gender" value="male" > Male
-            <input type="radio" name="gender" value="female"> Female
-            <input type="checkbox" name="superpower" value="Fly"> fly<br>
-            <input type="checkbox" name="superpower" value="fire"> fire<br>
-            <input type="submit" value="Submit">
-        </form>
 
         <?php
         $query = $_GET['query'];
@@ -41,7 +26,7 @@
                     $sql_query = "SELECT * FROM marvelMovies? WHERE yearReleased > 2010";
                     break;
                 case "oscar":
-                    $sql_query = "SELECT * FROM marvelMovies? WHERE notes LIKE '%'.$query.'%'";
+                    $sql_query = "SELECT * FROM marvelMovies? WHERE notes LIKE '%oscar%'";
                     break;
                 case "marvelstudio":
                     $sql_query = "SELECT * FROM marvelMovies? WHERE productionStudio = 'Marvel Studio'";
