13a8a736c5f46318fa8aa2cc55705c1d5c4fb91d
TheBlackBloodyUnicorn <TheBlackBloodyUnicorn@hotmail.com>
2015-10-26 14:23:07 +0000

    couocu

--- helloSector.php
+++ helloSector.php
@@ -10,8 +10,8 @@
         <p><a href="http://blackunicorn.azurewebsites.net/helloPrinter.php?query=Uranus">Uranus</a></p>
 
         <form action="helloPrinter.php" method="post">
-            <label>Forename</label><input type=“text” name=“forename”>
-            <label>Surname</label><input type=“text” name=“surname”>
+            <label>Forename</label><input type="text" name="forename">
+            <label>Surname</label><input type="text" name="surname">
             <input type="radio" name="gender" value="male" > Male
             <input type="radio" name="gender" value="female"> Female
             <input type="checkbox" name="superpower" value="Fly"> fly<br>
