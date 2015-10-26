4ef5caa4a3a770dca30ce10bb67745ce87f6b404
TheBlackBloodyUnicorn <TheBlackBloodyUnicorn@hotmail.com>
2015-10-26 14:17:56 +0000

    couocu

--- helloSector.php
+++ helloSector.php
@@ -8,7 +8,18 @@
         <p><a href="http://blackunicorn.azurewebsites.net/helloPrinter.php?query=Mars">Mars</a></p>
         <p><a href="http://blackunicorn.azurewebsites.net/helloPrinter.php?query=Earth">Earth</a></p>
         <p><a href="http://blackunicorn.azurewebsites.net/helloPrinter.php?query=Uranus">Uranus</a></p>
-    <?php
+
+        <form action="helloPrinter.php" method="post">
+            <label>Forename</label><input type=“text” name=“forename”>
+            <label>Surname</label><input type=“text” name=“surname”>
+            <input type="radio" name="gender" value="male" checked> Male
+            <input type="radio" name="gender" value="female"> Female
+            <input type="checkbox" name="superpower" value="Fly"> I have a bike<br>
+            <input type="checkbox" name="superpower" value="fire"> I have a car<br>
+            <input type="submit" value="Submit">
+        </form>
+
+        <?php
 
 
 
