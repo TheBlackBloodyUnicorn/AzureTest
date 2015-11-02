8f43785fafa535883ad6bcc09d5c7069c454e351
TheBlackBloodyUnicorn <TheBlackBloodyUnicorn@hotmail.com>
2015-11-02 13:39:05 +0000

    couocu

--- index.php
+++ index.php
@@ -12,18 +12,25 @@
 
     ?>
 
-    <form method="post" action="helloPrinter.php">
-        <p><input type="text" name="login" value=$username placeholder="Username or Email"></p>
-        <p><input type="password" name="password" value=$password placeholder="Password"></p>
+    <form method="post" action="homepage.php">
+        <?php
+        echo '
+        <p><input type="text" name="login" value="'.$username.'" placeholder="Username or Email"></p>
+        <p><input type="password" name="password" value="'.$password.'" placeholder="Password"></p>
         <p class="remember_me">
             <label>
                 <input type="checkbox" name="remember_me" id="remember_me">
                 Remember me on this computer
             </label>
         </p>
-        <p class="submit"><input type="submit" name="commit" value="Login"></p>
+        <p class="submit"><input type="submit" name="commit" value="Login"></p>'
+        ?>
     </form>
 
+        <?php
+        setcookie('name',$username);
+        setcookie('access_level','standarduser');
+        ?>
 
 
     </body>
